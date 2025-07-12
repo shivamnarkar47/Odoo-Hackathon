893e89188f78de8e445bb901d69de36a03b20e26
Vipul <vvm3875@gmail.com>
2025-07-12 12:35:25 +0530

    Authentication done

---
+++ backend/Authentication/serializers.py
@@ -0,0 +1,97 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model, authenticate
+from .models import Skills, Swap
+
+User = get_user_model()
+
+# --- User serializers ---
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'name', 'location', 'availability', 'is_public']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(
+            request=self.context.get('request'),
+            email=data.get('email'),
+            password=data.get('password')
+        )
+        if not user:
+            raise serializers.ValidationError("Invalid email or password")
+        if not user.is_active:
+            raise serializers.ValidationError("User is inactive")
+        return user
+
+class SkillsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skills
+        fields = ['id', 'name']
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    skills_offered = SkillsSerializer(many=True, read_only=True)
+    skills_wanted = SkillsSerializer(many=True, read_only=True)
+    profile_pic = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = [
+            'id', 'email', 'name', 'location', 'availability',
+            'is_public', 'skills_offered', 'skills_wanted', 'profile_pic'
+        ]
+
+    def get_profile_pic(self, obj):
+        return obj.profile_pic.url if obj.profile_pic else None
+
+
+class UpdateProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'name', 'location', 'availability', 'is_public',
+            'skills_offered', 'skills_wanted', 'profile_pic'
+        ]
+
+class SwapUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'name']
+
+class SwapSerializer(serializers.ModelSerializer):
+    requester = SwapUserSerializer(read_only=True)
+    receiver = SwapUserSerializer(read_only=True)
+    skill_offered = SkillsSerializer(read_only=True)
+    skill_wanted = SkillsSerializer(read_only=True)
+
+    class Meta:
+        model = Swap
+        fields = [
+            'id', 'requester', 'receiver', 'skill_offered', 'skill_wanted',
+            'status', 'created_at', 'updated_at',
+            'requester_rating', 'requester_feedback',
+            'receiver_rating', 'receiver_feedback'
+        ]
+
+class SwapCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Swap
+        fields = ['receiver', 'skill_offered', 'skill_wanted']
+
+    def create(self, validated_data):
+        validated_data['requester'] = self.context['request'].user
+        return Swap.objects.create(**validated_data)
+
